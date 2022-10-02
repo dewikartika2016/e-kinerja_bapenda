d7c16dd0a67191d2f90aaa6fd34378d66e9e0457
dewikartika2016 <1911522016_dewi@student.unand.ac.id>
2022-10-02 18:00:19 +0700

    coba pop up

--- login.php
+++ login.php
@@ -27,6 +27,7 @@
       $_SESSION['id'] = $data['id'];
       $_SESSION['name'] = $data['nama'];
       
+      // echo "<script>'contoh()';window.location.href='index.php'</script>";
       echo "<script>alert('Login berhasil!');window.location.href='index.php'</script>";  
     } else {
       echo "<script>alert('Username atau Password Salah')</script>";
@@ -60,6 +61,9 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
+  <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
   <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
@@ -69,6 +73,7 @@
   <link rel="stylesheet" href="assets/dist/css/AdminLTE.min.css">
   <!-- iCheck -->
   <link rel="stylesheet" href="assets/plugins/iCheck/square/blue.css">
+  <!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> -->
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -106,10 +111,26 @@
       </div>
     </form>
 
+    <!-- <div class="alert alert-danger" role="alert">Username atau password salah</div> -->
   </div>
   <!-- /.login-box-body -->
 </div>
 <!-- /.login-box -->
+
+<!-- <script type="text/javascript">
+        function contoh() {
+           swal({
+                title: "Berhasil!",
+                text: "Pop-up berhasil ditampilkan",
+                icon: "success",
+                button: true
+            });
+        }
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script> -->
 
 <!-- jQuery 2.2.3 -->
 <script src="assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
