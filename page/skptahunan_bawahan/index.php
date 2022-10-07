830f03e5136683cf26923a8da785eea7f2fa651e
Lisca Angriani <1911521012_lisca@student.unand.ac.id>
2022-10-07 11:30:56 +0700

    tes push git hub bro

---
+++ page/skptahunan_bawahan/index.php
@@ -0,0 +1,67 @@
+<?php 
+  if (isset($_GET['delete'])) {
+    $id_skptahunan = $_GET['id_skptahunan'];
+    $sql = "delete from skptahunan where id_skptahunan='$id_skptahunan'";
+    $query = mysqli_query($con, $sql);
+    if ($query) {
+      echo "<script>alert('Data SKP Tahunan Berhasil Dihapus!');window.location.href='detail_index.php?p=skptahunan'</script>";
+    } else {
+      echo mysqli_error($con);
+    }
+  }
+ ?>
+
+<div class="row">
+    <div class="col-xs-12">
+
+      <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">Data SKP Tahunan</h3><span class="pull-right"><a class="btn btn-success" href="?p=skptahunan&act=create">Tambah Data</a></span>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+          <table id="example1" class="table table-bordered table-striped">
+            <thead>
+            <tr>
+              <th>No</th>
+              <th>Target</th>
+              <th>Realisasi</th>
+              <th>Nama</th>
+              <th>Periode</th>
+              <th>Aksi</th>
+            </tr>
+            </thead>
+            <tbody>
+            	<?php 
+
+                $no = 0;
+            		$sql = "select * from skptahunan";
+            		$query = mysqli_query($con, $sql);
+            		while ($row = mysqli_fetch_assoc($query)):
+                  $no++;
+            	 ?>
+            	<tr>
+            	<td><?= $no ?></td>
+            	<td><?= date("d M Y", strtotime($row['periode_awal'])) ?> - <?= date("d M Y", strtotime($row['periode_akhir'])) ?></td>
+                <td><?= $row['kegiatan_tahunan'] ?></td>
+                <td><?= $row['target_kuantitas'] ?> <?= $row['satuan_kuantitas'] ?></td>
+                <td><?= $row['target_kualitas'] ?></td>
+                <td><?= $row['target_waktu'] ?></td>
+                <td><?= $row['biaya'] ?></td>
+                <td>
+                  <a href="detail_index.php?p=skptahunan&act=edit&id_skptahunan=<?= $row['id_skptahunan'] ?>" class="btn btn-primary"><i class="glyphicon glyphicon-edit"></i></a>
+                  <a href="detail_index.php?p=skptahunan&delete&id_skptahunan=<?= $row['id_skptahunan'] ?>" class="btn btn-danger" onclick="return confirm('Apakah Yakin Ingin Menghapus Data Atasan?')"><i class="glyphicon glyphicon-trash"></i></a>
+                </td>
+            	 </tr>
+            	<?php endwhile; ?>
+            </tbody>
+            
+          </table>
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+    </div>
+    <!-- /.col -->
+  </div>
+  <!-- /.row -->
