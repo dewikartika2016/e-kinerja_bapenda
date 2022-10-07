015424d6798090faba12ed06544f54587d888e4c
dewikartika2016 <1911522016_dewi@student.unand.ac.id>
2022-10-07 11:04:18 +0700

    db

--- config/ekinerja.sql
+++ config/ekinerja.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 05 Okt 2022 pada 06.56
+-- Waktu pembuatan: 07 Okt 2022 pada 06.03
 -- Versi server: 10.4.21-MariaDB
 -- Versi PHP: 8.0.10
 
@@ -31,17 +31,20 @@ CREATE TABLE `atasan` (
   `id_atasan` int(11) NOT NULL,
   `nip` varchar(50) NOT NULL,
   `nama_atasan` varchar(255) NOT NULL,
-  `jabatan` varchar(50) NOT NULL,
-  `bidang` varchar(50) NOT NULL
+  `id_jabatan` int(11) NOT NULL,
+  `id_unit_kerja` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data untuk tabel `atasan`
+-- Struktur dari tabel `jabatan`
 --
 
-INSERT INTO `atasan` (`id_atasan`, `nip`, `nama_atasan`, `jabatan`, `bidang`) VALUES
-(1, '18278478364', 'jahdgfy', 'Staff', 'Divisi Pemasaran'),
-(2, '13762836', 'ahhduerd', 'Kepala Bidang', 'Divisi Sumber Daya Manusia');
+CREATE TABLE `jabatan` (
+  `id_jabatan` int(11) NOT NULL,
+  `jabatan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -74,25 +77,84 @@ CREATE TABLE `kinerja` (
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `laporan_harian`
+--
+
+CREATE TABLE `laporan_harian` (
+  `id_laporan_harian` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `kegiatan_harian` varchar(125) NOT NULL,
+  `id_skp_bulanan` int(11) NOT NULL,
+  `kuantitas` int(11) NOT NULL,
+  `satuan_kuantitas` varchar(20) NOT NULL,
+  `status_laporan_harian` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `pegawai`
 --
 
 CREATE TABLE `pegawai` (
   `id_pegawai` int(11) NOT NULL,
   `nama_pegawai` varchar(255) NOT NULL,
-  `jabatan` varchar(125) NOT NULL,
-  `bidang` varchar(125) NOT NULL,
+  `id_jabatan` int(11) NOT NULL,
+  `id_unit_kerja` int(11) NOT NULL,
   `id_atasan` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data untuk tabel `pegawai`
+-- Struktur dari tabel `skp_bulanan`
 --
 
-INSERT INTO `pegawai` (`id_pegawai`, `nama_pegawai`, `jabatan`, `bidang`, `id_atasan`) VALUES
-(3, 'lwlkejfir', 'Kepala Seksi', 'Divisi Sumber Daya Manusia', 2),
-(5, 'jkfujkfwe', 'Staff', 'Divisi Sumber Daya Manusia', 1),
-(6, 'ajukdhuef', 'Staff', 'Divisi Pemasaran', 2);
+CREATE TABLE `skp_bulanan` (
+  `id_skp_bulanan` int(11) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `bulan` varchar(20) NOT NULL,
+  `id_skp_tahunan` int(11) NOT NULL,
+  `kegiatan_bulanan` varchar(125) NOT NULL,
+  `target_kuantitas` int(11) NOT NULL,
+  `satuan_kuantitas` varchar(20) NOT NULL,
+  `target_waktu` varchar(20) NOT NULL,
+  `biaya` int(11) NOT NULL,
+  `status_skp_bulanan` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `skp_tahunan`
+--
+
+CREATE TABLE `skp_tahunan` (
+  `id_skp_tahunan` int(11) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `kegiatan_tahunan` varchar(125) NOT NULL,
+  `target_kuantitas` varchar(20) NOT NULL,
+  `satuan_kuantitas` varchar(20) NOT NULL,
+  `target_kualitas` int(11) NOT NULL,
+  `target_waktu` varchar(20) NOT NULL,
+  `periode_awal` date NOT NULL,
+  `periode_akhir` date NOT NULL,
+  `biaya` varchar(50) NOT NULL,
+  `status_skp_tahunan` varchar(20) NOT NULL,
+  `id_pegawai` int(11) NOT NULL,
+  `id_atasan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `unit_kerja`
+--
+
+CREATE TABLE `unit_kerja` (
+  `id_unit_kerja` int(11) NOT NULL,
+  `unit_kerja` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -126,7 +188,15 @@ INSERT INTO `users` (`id_user`, `nama`, `username`, `password`, `email`, `no_tel
 -- Indeks untuk tabel `atasan`
 --
 ALTER TABLE `atasan`
-  ADD PRIMARY KEY (`id_atasan`);
+  ADD PRIMARY KEY (`id_atasan`),
+  ADD KEY `id_jabatan` (`id_jabatan`),
+  ADD KEY `id_bidang` (`id_unit_kerja`);
+
+--
+-- Indeks untuk tabel `jabatan`
+--
+ALTER TABLE `jabatan`
+  ADD PRIMARY KEY (`id_jabatan`);
 
 --
 -- Indeks untuk tabel `kegiatan`
@@ -144,11 +214,41 @@ ALTER TABLE `kinerja`
   ADD KEY `id_pegawai` (`id_pegawai`);
 
 --
+-- Indeks untuk tabel `laporan_harian`
+--
+ALTER TABLE `laporan_harian`
+  ADD PRIMARY KEY (`id_laporan_harian`),
+  ADD KEY `id_skp_bulanan` (`id_skp_bulanan`);
+
+--
 -- Indeks untuk tabel `pegawai`
 --
 ALTER TABLE `pegawai`
   ADD PRIMARY KEY (`id_pegawai`),
-  ADD KEY `atasan` (`id_atasan`);
+  ADD KEY `atasan` (`id_atasan`),
+  ADD KEY `id_unit_kerja` (`id_unit_kerja`),
+  ADD KEY `id_jabatan` (`id_jabatan`);
+
+--
+-- Indeks untuk tabel `skp_bulanan`
+--
+ALTER TABLE `skp_bulanan`
+  ADD PRIMARY KEY (`id_skp_bulanan`),
+  ADD KEY `id_skp_tahunan` (`id_skp_tahunan`);
+
+--
+-- Indeks untuk tabel `skp_tahunan`
+--
+ALTER TABLE `skp_tahunan`
+  ADD PRIMARY KEY (`id_skp_tahunan`),
+  ADD KEY `id_pegawai` (`id_pegawai`),
+  ADD KEY `id_atasan` (`id_atasan`);
+
+--
+-- Indeks untuk tabel `unit_kerja`
+--
+ALTER TABLE `unit_kerja`
+  ADD PRIMARY KEY (`id_unit_kerja`);
 
 --
 -- Indeks untuk tabel `users`
@@ -168,6 +268,12 @@ ALTER TABLE `atasan`
   MODIFY `id_atasan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT untuk tabel `jabatan`
+--
+ALTER TABLE `jabatan`
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT untuk tabel `kegiatan`
 --
 ALTER TABLE `kegiatan`
@@ -180,10 +286,34 @@ ALTER TABLE `kinerja`
   MODIFY `id_kinerja` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT untuk tabel `laporan_harian`
+--
+ALTER TABLE `laporan_harian`
+  MODIFY `id_laporan_harian` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT untuk tabel `pegawai`
 --
 ALTER TABLE `pegawai`
   MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `skp_bulanan`
+--
+ALTER TABLE `skp_bulanan`
+  MODIFY `id_skp_bulanan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `skp_tahunan`
+--
+ALTER TABLE `skp_tahunan`
+  MODIFY `id_skp_tahunan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `unit_kerja`
+--
+ALTER TABLE `unit_kerja`
+  MODIFY `id_unit_kerja` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
@@ -194,6 +324,13 @@ ALTER TABLE `users`
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
+
+--
+-- Ketidakleluasaan untuk tabel `atasan`
+--
+ALTER TABLE `atasan`
+  ADD CONSTRAINT `atasan_ibfk_1` FOREIGN KEY (`id_unit_kerja`) REFERENCES `unit_kerja` (`id_unit_kerja`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `atasan_ibfk_2` FOREIGN KEY (`id_jabatan`) REFERENCES `jabatan` (`id_jabatan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `kegiatan`
@@ -209,10 +346,29 @@ ALTER TABLE `kinerja`
   ADD CONSTRAINT `kinerja_ibfk_2` FOREIGN KEY (`id_kegiatan`) REFERENCES `kegiatan` (`id_kegiatan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Ketidakleluasaan untuk tabel `laporan_harian`
+--
+ALTER TABLE `laporan_harian`
+  ADD CONSTRAINT `laporan_harian_ibfk_1` FOREIGN KEY (`id_skp_bulanan`) REFERENCES `skp_bulanan` (`id_skp_bulanan`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Ketidakleluasaan untuk tabel `pegawai`
 --
 ALTER TABLE `pegawai`
-  ADD CONSTRAINT `pegawai_ibfk_2` FOREIGN KEY (`id_atasan`) REFERENCES `atasan` (`id_atasan`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `pegawai_ibfk_2` FOREIGN KEY (`id_atasan`) REFERENCES `atasan` (`id_atasan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pegawai_ibfk_3` FOREIGN KEY (`id_unit_kerja`) REFERENCES `unit_kerja` (`id_unit_kerja`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `skp_bulanan`
+--
+ALTER TABLE `skp_bulanan`
+  ADD CONSTRAINT `skp_bulanan_ibfk_1` FOREIGN KEY (`id_skp_tahunan`) REFERENCES `skp_tahunan` (`id_skp_tahunan`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `skp_tahunan`
+--
+ALTER TABLE `skp_tahunan`
+  ADD CONSTRAINT `skp_tahunan_ibfk_1` FOREIGN KEY (`id_pegawai`) REFERENCES `pegawai` (`id_pegawai`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
